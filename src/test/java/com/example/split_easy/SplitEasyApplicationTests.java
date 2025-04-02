9d0fff85d1578a280712b2ea49fc97d9526a0d1d
Maks <petliukofficial@gmail.com>
2025-04-02 13:59:22 +0300

    creating the project "split_easy"

---
+++ src/test/java/com/example/split_easy/SplitEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.split_easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
